fe56067627fcbcc82be053154318775059ed99d1
hongsokan_mac <hongsokan@daum.net>
2021-02-02 08:09:39 +0900

    0202

--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -4,4 +4,15 @@ const showcase = document.querySelector('.showcase');
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
+
+  // if (showcase.classList.toggle('active') === true) {
+  //   console.log('true');
+  // }
+  // else {
+  //   console.log('false');
+  // }
+  
+  // if (menuToggle.classList.toggle('active') === true) {
+  //   document.querySelector('.menu ul').setAttribute( 'display', 'block');
+  // }
 })
