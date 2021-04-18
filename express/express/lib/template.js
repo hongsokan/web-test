aac5f5a1c79d34be9b6042e2e928470d914b1280
hongsokan_mac <hongsokan@daum.net>
2021-04-19 08:42:58 +0900

    0419

--- express/express/lib/template.js
+++ express/express/lib/template.js
@@ -19,6 +19,42 @@ module.exports = {
     var list = '<ul>';
     var i = 0;
     while(i < filelist.length){
+      // express 적용 전 <a href="/?id=${filelist[i]}">
+      // express 적용 후 변경된 주소 체계
+      list = list + `<li><a href="/page/${filelist[i]}">${filelist[i]}</a></li>`;
+      i = i + 1;
+    }
+    list = list+'</ul>';
+    return list;
+  }
+}
+
+
+
+
+/*
+// express 적용 전
+module.exports = {
+  HTML:function(title, list, body, control){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>
+    `;
+  },list:function(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
       list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
       i = i + 1;
     }
@@ -26,3 +62,4 @@ module.exports = {
     return list;
   }
 }
+*/
